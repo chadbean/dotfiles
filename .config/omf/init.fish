e2957b0570089c6dc599382daea42b6d74aeafa0
Chad Bean <mail@chadbean.com>
2016-11-13 18:38:33 -0500

    Init commit

---
+++ .config/omf/init.fish
@@ -0,0 +1,14 @@
+if test -d /usr/local/share/chruby
+  source /usr/local/share/chruby/chruby.fish
+end
+
+set -gx LSCOLORS Exfxcxdxbxegedabagacad
+set -gx GOPATH ~/Dev/go
+set -gx EDITOR "code"
+
+set -U fish_user_paths ~/bin
+set -U fish_user_paths /usr/local/sbin $fish_user_paths
+set -U fish_user_paths $GOPATH/bin $fish_user_paths
+set -U fish_user_paths ~/Ops/tfenv/bin $fish_user_paths
+
+alias updatedb "sudo /usr/libexec/locate.updatedb"
